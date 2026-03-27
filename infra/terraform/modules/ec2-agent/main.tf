9f17a62a8f2cc577afd42ca85f1213b6c394af96
Christian <Christian>
2026-03-27 14:18:30 +0200

    allow any ip try to connect ec2, because github action needs to ssh inside it

--- infra/terraform/modules/ec2-agent/main.tf
+++ infra/terraform/modules/ec2-agent/main.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "this" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["87.95.220.168/32"] # can access only from my ip
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
